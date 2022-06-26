75a0a202d083ee4dee6a4a269b8e4c4f907d59f4
franklin-instantteams <frank.lin@instantteams.com>
2022-06-26 15:39:10 -0400

    refactored for cleanliness

--- components/home/body.tsx
+++ components/home/body.tsx
@@ -1,60 +1,16 @@
 import { Grid } from '@mui/material';
-import * as contentful from 'contentful';
-import { get } from 'lodash';
-import { useEffect, useState } from 'react';
 // jotai
-import { atom, useAtom } from 'jotai';
+import { useAtom } from 'jotai';
 import { homeBodyAtom } from '../../atoms/atoms';
 
-const INTRO_ENTRY_ID = '4Wx6Wy6m7uxp6NpKIf4LEB';
-
-const getParagraphs = (content: any) => {
-  const ret: any[] = [];
-  content.forEach((contentItem: { content: any }) => {
-    console.log(contentItem);
-    const contentItemContens = contentItem.content;
-    contentItemContens.forEach((contentItemContent: { value: any }) => {
-      ret.push(contentItemContent.value);
-    });
-  });
-
-  console.log(ret);
-  return ret;
-};
-
-const Body: any = (props: any) => {
-  const [homeBody, setHomeBody] = useState(['']);
-  const [jotaiHomeBody, setJotaiHomeBody] = useAtom(homeBodyAtom);
-
-  const getContent = async () => {
-    const params = {
-      space: `${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID}`,
-      accessToken: `${process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN}`
-    };
-    const client = contentful.createClient(params);
-    console.log('params: ', params);
-    const data = await client.getEntry(INTRO_ENTRY_ID);
-    const body = get(data, 'fields.body.content', undefined);
-    const formattedBody: string[] = getParagraphs(body);
-    setHomeBody(formattedBody);
-    setJotaiHomeBody(formattedBody);
-  };
-
-  useEffect(() => {
-    (async () => {
-      await getContent();
-    })();
-  }, []);
+const Body: any = () => {
+  const [jotaiHomeBody] = useAtom(homeBodyAtom);
 
   return (
     <Grid container>
       <Grid item xs={2} p={1} />
       <Grid item xs={8} p={1}>
         <div className="body-content-wrapper">
-          {homeBody.map((paragraph: string) => {
-            return <p key={Math.random()}>{paragraph}</p>;
-          })}
-          <hr />
           {jotaiHomeBody.map((paragraph: string) => {
             return <p key={Math.random()}>{paragraph}</p>;
           })}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,56 @@
 import type { NextPage } from 'next';
-import { Grid } from '@mui/material';
 import HTMLHead from '../components/head';
 import Header from '../components/header';
 import Menu from '../components/menu';
 import Body from '../components/home/body';
 import Footer from '../components/footer';
 import React from 'react';
+import { useEffect } from 'react';
+import { get } from 'lodash';
+
+// jotai
+import { useAtom } from 'jotai';
+import { homeBodyAtom } from '../atoms/atoms';
+
+// contentful
+import * as contentful from 'contentful';
+const INTRO_ENTRY_ID = '4Wx6Wy6m7uxp6NpKIf4LEB';
+
+const getParagraphs = (content: any) => {
+  const ret: any[] = [];
+  content.forEach((contentItem: { content: any }) => {
+    console.log(contentItem);
+    const contentItemContens = contentItem.content;
+    contentItemContens.forEach((contentItemContent: { value: any }) => {
+      ret.push(contentItemContent.value);
+    });
+  });
+
+  console.log(ret);
+  return ret;
+};
 
 const Home: NextPage = () => {
+  const [, setJotaiHomeBody] = useAtom(homeBodyAtom);
+
+  const getContent = async () => {
+    const params = {
+      space: `${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID}`,
+      accessToken: `${process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN}`
+    };
+    const client = contentful.createClient(params);
+    const data = await client.getEntry(INTRO_ENTRY_ID);
+    const body = get(data, 'fields.body.content', undefined);
+    const formattedBody: string[] = getParagraphs(body);
+    setJotaiHomeBody(formattedBody);
+  };
+
+  useEffect(() => {
+    (async () => {
+      await getContent();
+    })();
+  }, []);
+
   return (
     <>
       <HTMLHead />
